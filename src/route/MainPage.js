a0765c5e86a99bf44189fea7bf89c58e3d76e176
limjongseung <101609328+limjongseung@users.noreply.github.com>
2023-11-08 08:22:22 +0900

    sass modi

--- src/route/MainPage.js
+++ src/route/MainPage.js
@@ -1,21 +1,24 @@
 import { useNavigate } from "react-router-dom";
-
+import "../assets/scss/main.scss";
 function MainPage(props) {
-    let navigate = useNavigate();
-    return (
-      <div>
-        <button
-          onClick={() => {
-            navigate("/testpage");
-            props.setEI(0);
-            props.setSN(0);
-            props.setTF(0);
-            props.setJP(0);
-          }}>
-          시작
-        </button>
-      </div>
-    );
-  }
-  
-  export default MainPage;
+	let navigate = useNavigate();
+	return (
+		<div className="main_page">
+			<h1>찰떡의 MBTI</h1>
+			<button
+				className="buttonComponent"
+				onClick={() => {
+					navigate("/testpage");
+					props.setEI(0);
+					props.setSN(0);
+					props.setTF(0);
+					props.setJP(0);
+				}}
+			>
+				시작
+			</button>
+		</div>
+	);
+}
+
+export default MainPage;
