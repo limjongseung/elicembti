9224ba0ee884321438cf02d255ef368ec3295c0c
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-11-08 20:42:30 +0900

    scss

--- src/route/MainPage.js
+++ src/route/MainPage.js
@@ -1,24 +1,26 @@
 import { useNavigate } from "react-router-dom";
 import "../assets/scss/main.scss";
 function MainPage(props) {
-	let navigate = useNavigate();
-	return (
-		<div className="main_page">
-			<h1>찰떡의 MBTI</h1>
-			<button
-				className="buttonComponent"
-				onClick={() => {
-					navigate("/testpage");
-					props.setEI(0);
-					props.setSN(0);
-					props.setTF(0);
-					props.setJP(0);
-				}}
-			>
-				시작
-			</button>
-		</div>
-	);
+  let navigate = useNavigate();
+  return (
+    <div className="main_page">
+      <div className="main_page_wrapper">
+        <h1>나의 MBTI는?</h1>
+        <button
+          className="buttonComponent"
+          onClick={() => {
+            navigate("/testpage");
+            props.setEI(0);
+            props.setSN(0);
+            props.setTF(0);
+            props.setJP(0);
+          }}
+        >
+          시작
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
