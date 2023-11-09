d1416a8d8372795e30c81e02d60a8c624d374e2a
limjongseung <101609328+limjongseung@users.noreply.github.com>
2023-11-09 13:46:49 +0900

    final

--- src/route/ResultPage.js
+++ src/route/ResultPage.js
@@ -1,49 +1,124 @@
 import { useNavigate } from "react-router-dom";
-import React, {useState, useEffect} from 'react'
-
+import React, { useState, useEffect } from "react";
+import "../assets/scss/result.scss";
 function ResultPage(props) {
-  const whatMBTI = props.MBTI
-  const [res, setRes] = useState("");
+	const whatMBTI = props.MBTI;
+	const [res, setRes] = useState("");
 
-  useEffect(()=> {
-    if(whatMBTI) {setRes(whatMBTI)}
-  })
+	useEffect(() => {
+		if (whatMBTI) {
+			setRes(whatMBTI);
+		}
+	});
 
-  let navigate = useNavigate();
+	let navigate = useNavigate();
 
-  let data = {
-    ESTJ : {
-      subtitle : "당신은 ESTJ입니다.",
-      cont1 : "ESTJ는 아주 계획적이고 주도면밀합니다.",
-      cont2 : "INFP를 좋아합니다."
-    },
-    INFP : {
-      subtitle : "당신은 ESTJ입니다.",
-      cont1 : "ESTJ는 아주 계획적이고 주도면밀합니다.",
-      cont2 : "INFP를 좋아합니다."
-    }
-  }
+	let data = {
+		ISTJ: {
+			subtitle: "당신은 ISTJ입니다.",
+			cont1: "ESTJ는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ISTP: {
+			subtitle: "당신은 ISTP입니다.",
+			cont1: "ESTJ는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ESTP: {
+			subtitle: "당신은 ESTP입니다.",
+			cont1: "ESTP는 아주 계획적이고 주도면밀합니다. 그리고 성격이 아주 좋습니다.",
+			cont2: "제 자신을 가장 좋아합니다.",
+		},
+		ESTJ: {
+			subtitle: "당신은 ESTJ입니다.",
+			cont1: "ESTJ는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ISFJ: {
+			subtitle: "당신은 ISFJ입니다.",
+			cont1: "ESTJ는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ISFP: {
+			subtitle: "당신은 ISFP입니다.",
+			cont1: "ESTJ는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ESFP: {
+			subtitle: "당신은 ESFP입니다.",
+			cont1: "ESTJ는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ESFJ: {
+			subtitle: "당신은 ESFJ입니다.",
+			cont1: "ESTJ는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		INFJ: {
+			subtitle: "당신은 INFJ입니다.",
+			cont1: "ESTJ는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		INFP: {
+			subtitle: "당신은 ESTJ입니다.",
+			cont1: "ESTJ는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ENFP: {
+			subtitle: "당신은 ENFP입니다.",
+			cont1: "ENFP는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ENFJ: {
+			subtitle: "당신은 ENFJ입니다.",
+			cont1: "ENFP는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		INTJ: {
+			subtitle: "당신은 INTJ입니다.",
+			cont1: "ENFP는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		INTP: {
+			subtitle: "당신은 INTP입니다.",
+			cont1: "ENFP는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ENTP: {
+			subtitle: "당신은 ENTP입니다.",
+			cont1: "ENFP는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+		ENTJ: {
+			subtitle: "당신은 ENTJ입니다.",
+			cont1: "ENFP는 아주 계획적이고 주도면밀합니다.",
+			cont2: "INFP를 좋아합니다.",
+		},
+	};
 
-  // res에는 결정된 MBTI값이 들어있습니다.
-  // res에 ESTJ라는 값이 들어있어요. => data[ESTJ].subtitle
-  return (
-    <>
-    {res && (
-      <div>
-        <div>{data[res].subtitle}</div>
-        <div>{data[res].cont1}</div>
-        <div>{data[res].cont2}</div>
-        <button
-        onClick={() => {
-          navigate("/");
-        }}>
-        다시하기
-        </button>
-      </div>
-    )
-  }
-  </>
-  )
+	// res에는 결정된 MBTI값이 들어있습니다.
+	// res에 ESTJ라는 값이 들어있어요. => data[ESTJ].subtitle
+	return (
+		<>
+			{res && (
+				<div className="result_wrapper">
+					<div className="result_page">
+						<h1>결과!</h1>
+						<div>{data[res].subtitle}</div>
+						<div>{data[res].cont1}</div>
+						<div>{data[res].cont2}</div>
+						<button
+							onClick={() => {
+								navigate("/");
+							}}
+						>
+							다시하기
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default ResultPage;
--- src/route/TestPage.js
+++ src/route/TestPage.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import React,{useState, useEffect} from 'react'
+import "../assets/scss/test.page.scss";
 
 function TestPage(props) {
   useEffect(()=> {
@@ -40,73 +41,74 @@ function TestPage(props) {
   const [num, setNum] = useState(1);
   const [data] = useState({
     1: {
-      ques: "친구 1년 못만나기 vs 장기자랑 10분하기",
-      ans1: "장기자랑 10분하기",
-      ans2: "친구 1년 못만나기"
+      ques: "생일 파티 초대할 수 있는 친구는?",
+      ans1: "어느 정도 친한 친구도 초대 가능",
+      ans2: "찐친 몇 명만 초대 가능"
     },
     2: {
-      ques: "EI 구분문제",
-      ans1: "E성향 답변",
-      ans2: "I성향 답변"
+      ques: "오랜만에 친구와 만나 놀던 중에, 친구의 친구가 같이 놀아도 되냐고 물어보는데?",
+      ans1: "당연히 되지! 오늘부터 너도 내 친구다.",
+      ans2: "어어..? (안돼, 절대싫어)"
     },
     3: {
-      ques: "EI 구분문제",
-      ans1: "E성향 답변",
-      ans2: "I성향 답변"
+      ques: "새 학기 첫날! 친한 사람이 한 명도 없는 반에서 나는",
+      ans1: "초면에 여기저기 말 걸었더니 어느새 주의에 사람으로 둘어 쌓여짐",
+      ans2: "누군가가 말 걸어 주길 기다려본다."
     },
     4: {
-      ques: "SN 구분문제",
-      ans1: "S성향 답변",
-      ans2: "N성향 답변"
+      ques: "노래 들을 때 더 좋아하는 것은?",
+      ans1: "멜로디",
+      ans2: "가사"
     },
     5: {
-      ques: "SN 구분문제",
-      ans1: "S성향 답변",
-      ans2: "N성향 답변"
+      ques: "아무 생각을 안 할 수 있다? 없다?",
+      ans1: "정말 아무 생각 안 하기 가능.",
+      ans2: "아무 생각을 안 하여해도 머릿속에 상상이 멈추지 않음"
     },
     6: {
-      ques: "SN 구분문제",
-      ans1: "S성향 답변",
-      ans2: "N성향 답변"
+      ques: "요리 할 때 정량을 지키는가? 손대중으로 하는가?",
+      ans1: "레시피에 나온대로 정량을 딱 맞춤.",
+      ans2: "손대중! 이 정도 넣으면 되겠지"
     },
     7: {
-      ques: "TF 구분문제",
-      ans1: "T성향 답변",
-      ans2: "F성향 답변"
+      ques: "난 니가 그냥 좋아!",
+      ans1: "나를? 왜?",
+      ans2: "!!(감동...)"
     },
     8: {
-      ques: "TF 구분문제",
-      ans1: "T성향 답변",
-      ans2: "F성향 답변"
+      ques: "나 너랑 별로 안 맞는 것 같아",
+      ans1: "왜?(이유가 궁금)",
+      ans2: "!!(마상..)"
     },
     9: {
-      ques: "TF 구분문제",
-      ans1: "T성향 답변",
-      ans2: "F성향 답변"
+      ques: "나 배탈 난 것 같아",
+      ans1: "뭐 먹었는데?",
+      ans2: "어떻게? 병원 갈까?"
     },
     10: {
-      ques: "JP 구분문제",
-      ans1: "J성향 답변",
-      ans2: "P성향 답변"
+      ques: "뭐하냐~ 심심한데 나와",
+      ans1: "응?.... 갑자기?",
+      ans2: "당근 좋지 당장 나갈게"
     },
     11: {
-      ques: "JP 구분문제",
-      ans1: "J성향 답변",
-      ans2: "P성향 답변"
+      ques: "어떤 회사가 좋아?",
+      ans1: "회사는 회사니깐 체계적이어야해",
+      ans2: "딱딱한 회사 별로 자유로운 회사지"
     },
     12: {
-      ques: "JP 구분문제",
-      ans1: "J성향 답변",
-      ans2: "P성향 답변"
+      ques: "오늘 뭐먹을까?",
+      ans1: "나에게 다 계획이 있어 맛집리스트 다 뽑았지",
+      ans2: "저기 맛있겠다 배고파"
     }
   })
 
   return (
-    <div>
-      <div>
+    <div className="test_page_wrapper">
+      <div className="test_page">
         Q : {data[num].ques}
       </div>
-      <button
+      <div className="button_wrapper">
+      <button 
         onClick={() => {
           setNum(num+1);
           if (num <= 3) {
@@ -142,6 +144,8 @@ function TestPage(props) {
         }}>
         {data[num].ans2}
       </button>
+
+      </div>
     </div>
   );
 }
